25217dc4303d6c78a524b2fbb4e6f851f5dfeb08
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-11-06 11:40:43 +0100

    type improvements

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-declare type Placement = (
+/* eslint-disable no-unused-vars */
+
+import { LiteralUnion, AnyCase } from '@brixtol/tsutils';
+import { CurrencyCodes } from '@brixtol/currency-codes';
+
+type Placement = (
   /**
    * Symbol First, eg: $100
    */
@@ -17,208 +22,27 @@ declare type Placement = (
   '# !'
 )
 
-interface IPlacements {
-  AED: Placement, // 100 د.إ
-  AFN: Placement, // ؋100
-  ALL: Placement, // 100 L
-  AMD: Placement, // 100֏
-  ANG: Placement,
-  AOA: Placement,
-  ARS: Placement,
-  AUD: Placement, // $100.00
-  AWG: Placement,
-  AZN: Placement,
-  BAM: Placement,
-  BBD: Placement,
-  BDT: Placement,
-  BGN: Placement,
-  BHD: Placement,
-  BIF: Placement,
-  BMD: Placement,
-  BND: Placement,
-  BOB: Placement,
-  BRL: Placement,
-  BSD: Placement,
-  BTC: Placement,
-  BTN: Placement,
-  BWP: Placement,
-  BYR: Placement,
-  BYN: Placement,
-  BZD: Placement,
-  CAD: Placement,
-  CDF: Placement,
-  CHF: Placement,
-  CLP: Placement,
-  CNY: Placement,
-  COP: Placement,
-  CRC: Placement,
-  CUC: Placement,
-  CUP: Placement,
-  CVE: Placement,
-  CZK: Placement,
-  DJF: Placement,
-  DKK: Placement,
-  DOP: Placement,
-  DZD: Placement,
-  EEK: Placement,
-  EGP: Placement,
-  ERN: Placement,
-  ETB: Placement,
-  ETH: Placement,
-  EUR: Placement,
-  FJD: Placement,
-  FKP: Placement,
-  GBP: Placement,
-  GEL: Placement,
-  GGP: Placement,
-  GHC: Placement,
-  GHS: Placement,
-  GIP: Placement,
-  GMD: Placement,
-  GNF: Placement,
-  GTQ: Placement,
-  GYD: Placement,
-  HKD: Placement,
-  HNL: Placement,
-  HRK: Placement,
-  HTG: Placement,
-  HUF: Placement,
-  IDR: Placement,
-  ILS: Placement,
-  IMP: Placement,
-  INR: Placement,
-  IQD: Placement,
-  IRR: Placement,
-  ISK: Placement,
-  JEP: Placement,
-  JMD: Placement,
-  JOD: Placement,
-  JPY: Placement,
-  KES: Placement,
-  KGS: Placement,
-  KHR: Placement,
-  KMF: Placement,
-  KPW: Placement,
-  KRW: Placement,
-  KWD: Placement,
-  KYD: Placement,
-  KZT: Placement,
-  LAK: Placement,
-  LBP: Placement,
-  LKR: Placement,
-  LRD: Placement,
-  LSL: Placement,
-  LTC: Placement,
-  LTL: Placement,
-  LVL: Placement,
-  LYD: Placement,
-  MAD: Placement,
-  MDL: Placement,
-  MGA: Placement,
-  MKD: Placement,
-  MMK: Placement,
-  MNT: Placement,
-  MOP: Placement,
-  MRO: Placement,
-  MRU: Placement,
-  MUR: Placement,
-  MVR: Placement,
-  MWK: Placement,
-  MXN: Placement,
-  MYR: Placement,
-  MZN: Placement,
-  NAD: Placement,
-  NGN: Placement,
-  NIO: Placement,
-  NOK: Placement,
-  NPR: Placement,
-  NZD: Placement,
-  OMR: Placement,
-  PAB: Placement,
-  PEN: Placement,
-  PGK: Placement,
-  PHP: Placement,
-  PKR: Placement,
-  PLN: Placement,
-  PYG: Placement,
-  QAR: Placement,
-  RMB: Placement,
-  RON: Placement,
-  RSD: Placement,
-  RUB: Placement,
-  RWF: Placement,
-  SAR: Placement,
-  SBD: Placement,
-  SCR: Placement,
-  SDG: Placement,
-  SEK: Placement,
-  SGD: Placement,
-  SHP: Placement,
-  SLL: Placement,
-  SOS: Placement,
-  SRD: Placement,
-  SSP: Placement,
-  STD: Placement,
-  STN: Placement,
-  SVC: Placement,
-  SYP: Placement,
-  SZL: Placement,
-  THB: Placement,
-  TJS: Placement,
-  TMT: Placement,
-  TND: Placement,
-  TOP: Placement,
-  TRL: Placement,
-  TRY: Placement,
-  TTD: Placement,
-  TVD: Placement,
-  TWD: Placement,
-  TZS: Placement,
-  UAH: Placement,
-  UGX: Placement,
-  USD: Placement,
-  UYU: Placement,
-  UZS: Placement,
-  VEF: Placement,
-  VND: Placement,
-  VUV: Placement,
-  WST: Placement,
-  XAF: Placement,
-  XBT: Placement,
-  XCD: Placement,
-  XOF: Placement,
-  XPF: Placement,
-  YER: Placement,
-  ZAR: Placement,
-  ZMW: Placement,
-  ZWD: Placement,
-}
-
-/**
- * Literal Union Helper
- *
- * Allows string types to be passed while respecting
- * intellisense completions.
- */
-type Union<T, B extends | null | undefined | string | symbol> = T | (B & {_?: never})
-
-/**
- * Object Values
- *
- * Creates a union of currency code values used as a return
- * type in function export.
- */
-type Values<T, V extends keyof T = keyof T> = T[V];
-
 /**
  * Currency Placement
  *
- * Extracts the placement string from the IPLacements interface
+ * Extracts the placement string from the IPlacements interface
  * which is use as the Return type reference
  */
-type CurrencyPlacement<ISO extends keyof IPlacements> = Values<IPlacements, ISO>
+type SymbolPlacements = { [K in CurrencyCodes]: Placement }
 
-const Placements: IPlacements = Object.freeze({
+/**
+ * Country Name
+ *
+ * Extracts the country name from the ICountries interface
+ * which is use as the Return type reference
+ */
+export type CurrencyPlacement<ISO> = (
+  ISO extends CurrencyCodes
+  ? SymbolPlacements[ISO]
+  : never
+)
+
+const Placements: SymbolPlacements = Object.freeze({
   AED: '# !', // 100 د.إ
   AFN: '#!', // ؋100
   ALL: '# !', // 100 L
@@ -391,8 +215,10 @@ const Placements: IPlacements = Object.freeze({
   XPF: '!#',
   YER: '!#',
   ZAR: '!#',
+  ZMK: '!#',
   ZMW: '!#',
-  ZWD: '!#'
+  ZWD: '!#',
+  ZWL: '!#'
 });
 
 /**
@@ -405,9 +231,9 @@ const Placements: IPlacements = Object.freeze({
  * > _Accepts either uppercase, lowercase or
  * or a combination of either_
  */
-function getPlacement <ISO extends keyof IPlacements> (
-  code: Union<ISO, string>
-): CurrencyPlacement<ISO> {
+function getPlacement <ISO extends AnyCase<CurrencyCodes>> (
+  code: LiteralUnion<ISO>
+): CurrencyPlacement<Uppercase<ISO>> {
 
   const placement = Placements[code.toUpperCase()];
 
@@ -417,4 +243,4 @@ function getPlacement <ISO extends keyof IPlacements> (
 
 }
 
-export { IPlacements, Placements, getPlacement };
+export { SymbolPlacements as IPlacements, Placements, getPlacement };
